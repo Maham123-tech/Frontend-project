1b8eebc3dbd575b2cc74c8a2e2b824ef43eef245
Maham_Fatima <mahamf452@gmail.com>
2025-07-31 20:04:42 +0500

    Update Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,8 +8,11 @@ const Navbar = () => {
       <nav className="nav-links">
         <a href="#" className="nav-link">About us</a>
         <a href="#" className="nav-link">Services</a>
-        <a href="#" className="nav-link">Projects</a>
+         <a href='#'className="nav-link">Case Studies</a>
+    
         <a href="#" className="nav-link">Blog</a>
+        <a href='#'className="nav-link">How it works</a>
+        <a href='#'className="nav-link">Hire</a>
       </nav>
       <button className="contact-btn">Contact us</button>
     </header>
